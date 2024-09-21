fefb4a773fb4d66c3d913b7359d9a26f091d2bc9
eliflali <26ninlaneti@gmail.com>
2024-09-21 18:11:01 +0300

    models complete bugs fixed

--- cagdas_yasam/etkinlik_takip/forms.py
+++ cagdas_yasam/etkinlik_takip/forms.py
@@ -1,29 +1,17 @@
-# forms.py
 from django import forms
-from .models import Member
+from .models import Member, Student, Volunteers, Registered
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-
 class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
-        fields = ['name', 'mission', 'school', 'department', 'student_class', 'group', 'phone_number', 'email', 'tc_number', 'active', 'points_collected']
-
-        #fields = ['İsim Soyisim', 'Görevi', 'Okul', 'Bölüm', 'Sınıfı', 'Öbek', 'Telefon Numarası', 'E-mail', 'TC Numarası', 'Hala Aktif', 'Toplam Puan']
-
-
+        fields = ['name', 'tc_number', 'total_volunteering_hours', 'start_time', 'end_time']
 
 class QueryMemberForm(forms.Form):
     name = forms.CharField(label='Ad', required=False)
-    mission = forms.CharField(label='Görev', required=False)
-    school = forms.CharField(label='Okul', required=False)
-    department = forms.CharField(label='Bölüm', required=False)
-    student_class = forms.CharField(label='Sınıf', required=False)
-    group = forms.CharField(label='Grup', required=False)
-    phone_number = forms.CharField(label='Telefon Numarası', required=False)
-    email = forms.EmailField(label='E-posta', required=False)
     tc_number = forms.CharField(label='TC Kimlik No', required=False)
-    # 'active' and 'points_collected' can be omitted or added depending on your search requirements
+    start_time = forms.DateTimeField(label='Başlangıç Zamanı', required=False)
+    end_time = forms.DateTimeField(label='Bitiş Zamanı', required=False)
--- cagdas_yasam/etkinlik_takip/models.py
+++ cagdas_yasam/etkinlik_takip/models.py
@@ -38,26 +38,26 @@ class Project(models.Model):
 class Member(models.Model):
     name = models.CharField(_("Ad"), max_length=100)
     tc_number = models.CharField(_("TC Kimlik No"), max_length=11)
-    total_volunteering_hours = models.IntegerField(_("Toplam Gönüllü Saat"), default=0)
-    departments = models.ManyToManyField(Department, through='Membership', related_name='members')
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    total_volunteering_hours = models.IntegerField(_("Toplam Gönüllü Saat"), default=0, blank = True)
+    departments = models.ManyToManyField(Department, through='Membership', related_name='members', blank = True)
+    start_time = models.DateTimeField(null = True)
+    end_time = models.DateTimeField(null = True)
 
     def __str__(self):
         return self.name
     
 class Volunteers(Member):
-    status = models.CharField(_("Durum (Burslu/Gönüllü)"), default="VolunteeringMember")
+    status = models.CharField(_("Durum (Burslu/Gönüllü)"), default="VolunteeringMember", max_length=100)
     
 class Registered(Member):
-    status = models.CharField(_("Durum (Burslu/Gönüllü)"), default="Member")
+    status = models.CharField(_("Durum (Burslu/Gönüllü)"), default="Member", max_length=100)
 
 class Membership(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     projects = models.ManyToManyField(Project, blank=True)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    start_time = models.DateTimeField(null = True)
+    end_time = models.DateTimeField(null = True)
 
     def __str__(self):
         return f"{self.member.name} - {self.department.name}"
@@ -70,11 +70,11 @@ class Student(models.Model):
     department = models.CharField(_("Bölüm"), max_length=100, blank=True)
     student_class = models.CharField(_("Sınıf"), max_length=50, blank=True)
     phone_number = models.CharField(_("Telefon Numarası"), max_length=20, blank=True)
-    active = models.BooleanField(_("Aktif"), default=True)
-    points_collected = models.IntegerField(_("Toplanan Puanlar"), default=0)
-    total_volunteering_hours = models.IntegerField(_("Toplam Gönüllü Saat"), default=0)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    active = models.BooleanField(_("Aktif"), default=True, blank = True)
+    points_collected = models.IntegerField(_("Toplanan Puanlar"), default=0, blank = True)
+    total_volunteering_hours = models.IntegerField(_("Toplam Gönüllü Saat"), default=0, blank = True)
+    start_time = models.DateTimeField(null = True)
+    end_time = models.DateTimeField(null = True)
 
     def __str__(self):
         return self.name
@@ -82,17 +82,27 @@ class Student(models.Model):
 class ScholarshipStudent(Student):
     mission = models.CharField(_("Görev"), max_length=100)
     group = models.CharField(_("Öbek"), max_length=100, blank=True)
-    status = models.CharField(_("Durum (Burslu/Gönüllü)"), default="ScholarshipStudent")
+    status = models.CharField(_("Durum (Burslu/Gönüllü)"), default="ScholarshipStudent", max_length=100)
 
     def __str__(self):
         return f"{self.name} - {self.status}"
 
 class VolunteeringStudent(Student):
-    status = models.CharField(_("Durum (Burslu/Gönüllü)"), default="VolunteeringStudent")
+    status = models.CharField(_("Durum (Burslu/Gönüllü)"), default="VolunteeringStudent", max_length=100)
 
     def __str__(self):
         return f"{self.name} - {self.status}"
 
+class StudentMembership(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    projects = models.ManyToManyField(Project, blank=True)
+    start_time = models.DateTimeField(null = True)
+    end_time = models.DateTimeField(null = True)
+
+    def __str__(self):
+        return f"{self.student.name} - {self.department.name}"
+    
 # Define the TargetGroup model
 class TargetGroup(models.Model):
     TARGET_GROUP_CHOICES = [
@@ -122,12 +132,12 @@ class TargetGroup(models.Model):
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
-    date = models.DateTimeField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    category = models.CharField(max_length=50)
-    explanation  = models.CharField(max_length=255)
-    event_type = models.CharField(max_lenght=50)
+    date = models.DateTimeField(blank = True)
+    start_time = models.DateTimeField(null = True)
+    end_time = models.DateTimeField(null = True)
+    category = models.CharField(max_length=50, default = "")
+    explanation  = models.CharField(max_length=255, null = True)
+    event_type = models.CharField(max_length=50, null = True)
     point = models.IntegerField()
     place = models.CharField(max_length=255, blank=True, null=True)
     manager_student = models.OneToOneField(
@@ -136,11 +146,11 @@ class Event(models.Model):
     manager_member = models.OneToOneField(
         'Member', on_delete=models.SET_NULL, null=True, blank=True, related_name='managed_events'
     )
-    target_groups = models.ManyToManyField(TargetGroup, related_name='events')  # Add this line
+    target_groups = models.ManyToManyField(TargetGroup, related_name='events', blank = True)  # Add this line
     native_departments = models.ManyToManyField(NativeDepartment, related_name='native_events', blank=True)
     helper_departments = models.ManyToManyField(HelperDepartment, related_name='helper_events', blank=True)
-    attendant_from_other = models.IntegerField()
-    attendant_from_us = models.IntegerField()
+    attendant_from_other = models.IntegerField(default = 0, blank = True)
+    attendant_from_us = models.IntegerField(default = 0, blank = True)
     def clean(self):
         # Ensuring only one manager is set
         if self.manager_student and self.manager_member:
